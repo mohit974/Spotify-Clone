fb27fab3ddda1f60b77e0b879145f809e7f42a99
mohitgandhi11 <mohitgandhi0011@gmail.com>
2023-11-26 17:53:10 +0530

    initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,46 @@
+import "./globals.css";
+import type { Metadata } from "next";
+import { Figtree } from "next/font/google";
+
+const font = Figtree({ subsets: ["latin"] });
+
+import Sidebar from "@/components/Sidebar";
+import Player from "@/components/Player";
+
+import SupabaseProvider from "@/providers/SupabaseProvider";
+import UserProvider from "@/providers/UserProvider";
+import ModalProvider from "@/providers/ModalProvider";
+import ToasterProvider from "@/providers/ToasterProvider";
+
+import getSongsByUserId from "@/actions/getSongsByUserId";
+import getActiveProductsWithPrices from "@/actions/getActiveProductsWithPrices";
+
+export const metadata: Metadata = {
+  title: "Spotify Clone",
+  description: "Stream Music!",
+};
+
+export const revalidate = 0;
+
+export default async function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const userSongs = await getSongsByUserId();
+  const products = await getActiveProductsWithPrices();
+  return (
+    <html lang="en">
+      <body className={font.className}>
+        <ToasterProvider />
+        <SupabaseProvider>
+          <UserProvider>
+            <ModalProvider products={products} />
+            <Sidebar songs={userSongs}>{children}</Sidebar>
+            <Player />
+          </UserProvider>
+        </SupabaseProvider>
+      </body>
+    </html>
+  );
+}
